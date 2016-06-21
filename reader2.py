519063f7e72df54ce1af0b73363a8fd32d5838bc
abhishek <abhishekmehta1992@gmail.com>
2016-06-21 19:14:22 +0530

    Generation of bucket, bucket to vector conversion and sample hierarchical cluster code

--- reader2.py
+++ reader2.py
@@ -2,133 +2,240 @@ import Document_pb2
 import re
 import pickle
 import sys  
+import numpy as np
+import os.path
+from matplotlib import pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage, cophenet
+from scipy.spatial.distance import pdist
+
+def loadGuidMap(refresh=False):
+    pickle_dump_path = "./data/interim/guidMap.p"
+    guid_file_path = "./data/raw/filtered-freebase-simple-topic-dump-3cols.tsv"
+    if refresh or not os.path.isfile(pickle_dump_path):
+        print "Creating new guid map pickle dump"
+        guidFile=open(guid_file_path,"r")
+        guidMap={}
+        pattern=r'(/guid/[a-zA-Z0-9]*)([^/]*).*'
+        for line in guidFile:
+            mo=re.match(pattern,line)
+            guid=mo.group(1)
+            ent=mo.group(2)
+            ent=ent.strip()
+            guidMap[guid]=ent
+        pickle.dump(guidMap,open(pickle_dump_path,"wb"))
+    else:
+        print "Using existing pickle dump for guidMap"
+        guidMap = pickle.load(open(pickle_dump_path, "rb"))
+
+    return guidMap
+
+def loadVectorMap(refresh=False):
+    pickle_dump_path = "./data/interim/vectorMap.p"
+    vector_file_path = "./data/raw/glove.6B/glove.6B.50d.txt"
+    if refresh or not os.path.isfile(pickle_dump_path):
+        print "Creating new vector map" 
+        vecFile= open(vector_file_path,"r")
+        vectorMap={}
+        for line in vecFile:
+            tokenList=line.split(" ") 
+            word = tokenList[0] 
+            vector = np.fromstring(' '.join(tokenList[1:]), sep =' ')
+            vectorMap[word]=vector
+        pickle.dump(vectorMap,open(pickle_dump_path,"wb"))
+    else:
+        print "Using existing vector map"
+        vectorMap = pickle.load(open(pickle_dump_path,"rb")) 
+    return vectorMap
+
+def loadRelationMap():
+    relidFile=open("./data/processed/relationIDs.tsv","r")
+    data = filter(None,relidFile.read().split('\n'))
+    relationMap = {}
+    for line in data:
+        temp = line.split('\t')
+        relationMap[temp[1]] = int(temp[0])
+    return relationMap
+
+def load_buckets(guidMap, relationMap, refresh=False):
+    """
+        return type : Dictionary object named buckets
+        buckets keys : file no. example 100000
+        buckets value : A list with the following mapping ->
+            0th index : entity 1
+            1st index : entity 2
+            2nd index : relation id
+            3rd index : list of pharases
+            4th index : sentence count
+    """
+    pickle_dump_path = "./data/interim/buckets.p"
+    sample_file_path = "./data/raw/kb_manual/trainPositive/"
+    sample_low_count = 100000
+    sample_high_count = 119465
+    if refresh or not os.path.isfile(pickle_dump_path):
+        print "creating bucket"
+        buckets = {}
+        for i in range(sample_low_count,sample_high_count):
+            inputFile= open(sample_file_path + str(i) + ".pb","rb")
+            rel=Document_pb2.Relation()
+            rel.ParseFromString(inputFile.read())
+            inputFile.close()
+            bucket_content = []
+            sentences = []
+            try:
+                entity_1 = guidMap[rel.sourceGuid]
+            except KeyError:
+                print "entity 1 Key Error"
+            try:
+                entity_2 = guidMap[rel.destGuid]
+            except KeyError:
+                print "entity 2 Key Error"
+            bucket_content.append(entity_1)
+            bucket_content.append(entity_2)
+            try: 
+                relation = relationMap[rel.relType]
+            except KeyError:
+                print "Relation Key Error"
+            bucket_content.append(relation)
+            phrases = []
+            count = len(rel.mention)
+            for mention in rel.mention:
+                phrase1=re.match("(.*)"+entity_1.decode('utf-8')+"(.*)"+entity_2.decode('utf-8')+"(.*)",mention.sentence, re.UNICODE)
+                phrase2=re.match("(.*)"+entity_2.decode('utf-8')+"(.*)"+entity_1.decode('utf-8')+"(.*)",mention.sentence, re.UNICODE)
+                if phrase1:
+                    phrase=phrase1.group(2)
+                elif phrase2:
+                    phrase=phrase2.group(2)
+                else:
+                    phrase = 'not-not-found'
+                    print "Error in finding phrase for", entity_1, entity_2, mention.sentence, rel.sourceGuid, rel.destGuid
+                phrases.append(phrase)
+            bucket_content.append(phrases)
+            bucket_content.append(count)
+            buckets[i] = bucket_content
+        pickle.dump(buckets, open(pickle_dump_path, "wb"))
+    else:
+        print "Using existing bucket"
+        buckets = pickle.load(open(pickle_dump_path, "rb"))
+    return buckets
+
+def bucket_to_vectors(buckets, vectorMap, refresh=False, remove_duplicate = False):
+    pickle_dump_path = "./data/interim/buckets_vectors.p"
+    if refresh or not os.path.isfile(pickle_dump_path):
+        print "creating bucket vector"
+        N = 121867 # max total no. of sentences in this dataset
+
+        # initialize vectors to max length N
+        counter = 0
+        entity_1_vectors = np.zeros((N,50), dtype=np.float32)
+        entity_2_vectors = np.zeros((N,50), dtype=np.float32)
+        phrase_vectors = np.zeros((N,50), dtype=np.float32)
+        label_vector = np.zeros(N)
+        phrases_list = []
+
+        for key in buckets.keys():
+            # discard phrases that contain a word not present in vectorMap using this flag
+            error_while_vectorization = False
+            data = buckets[key]
+
+            entity_1 = data[0].split(' ')
+            entity_2 = data[1].split(' ')
+           
+            # initialize entity vectors to zeros
+            vector_e1 = np.zeros(50, dtype=np.float32)
+            vector_e2 = np.zeros(50, dtype=np.float32)
+
+            for word in entity_1:
+                try:
+                    vector_e1 += vectorMap[word.lower()]
+                except KeyError:
+                    print "entity 1", word.lower(), "not found in vectorMap"
+                    error_while_vectorization = True        
+            vector_e1 /= len(entity_1)
+
+            for word in entity_2:
+                try:
+                    vector_e2 += vectorMap[word.lower()]
+                except KeyError:
+                    print "entity 2", word.lower(), "not found"
+                    error_while_vectorization = True        
+            vector_e2 /= len(entity_2)
+
+            for phrase in data[3]:
+                # initialize phrase vector to zero
+                vector_phrase = np.zeros(50, dtype=np.float32)
+
+                words = filter(None,phrase.split(' '))
+                for word in words:
+                    try:
+                        vector_phrase += vectorMap[word.lower()]
+                    except KeyError:
+                        print "Phrase", word.lower(), "not found"
+                        error_while_vectorization = True        
+                vector_phrase /= len(data[3])
+                if not error_while_vectorization:
+                    entity_1_vectors[counter] = vector_e1
+                    entity_2_vectors[counter] = vector_e2
+                    phrase_vectors[counter] = vector_phrase
+                    label_vector[counter] = data[2]
+                    phrases_list.append(phrase)
+                    counter += 1
+        print counter
+
+        # discard vectors indexes above counter count
+        entity_1_vectors = entity_1_vectors[0:counter]
+        entity_2_vectors = entity_2_vectors[0:counter]
+        phrase_vectors = phrase_vectors[0:counter]
+        label_vector = label_vector[0:counter]
+
+        phrases_label = np.array(phrases_list, dtype=object)
+
+        X_train = np.hstack((entity_1_vectors, entity_2_vectors, phrase_vectors))
+        pickle.dump((X_train, label_vector, phrases_label), open(pickle_dump_path, "wb"))
+    else:
+        print "Using existing bucket vector"
+        X_train, label_vector, phrases_label = pickle.load(open(pickle_dump_path, "rb"))
+    return X_train, label_vector, phrases_label
+
+if __name__ == "__main__":
+    guidMap=loadGuidMap()
+    vectorMap = loadVectorMap(refresh=False)
+    relationMap = loadRelationMap()
+    buckets = load_buckets(guidMap, relationMap, refresh=False)
+    X_train, y_label, phrases_label = bucket_to_vectors(buckets, vectorMap, refresh=False)
+   
+    # extract samples for one particular relation type
+    # id 2 = /people/person/place_of_birth
+    sample_idx = y_label == 2
+    X_sample = X_train[sample_idx]
+    phrase_sample = phrases_label[sample_idx]
 
 
+    # hierarchical clustering sample
+    # source https://joernhees.de/blog/2015/08/26/scipy-hierarchical-clustering-and-dendrogram-tutorial/
 
+    phrase_vectors = X_sample[:,50:100]
+    Z = linkage(phrase_vectors, 'single', metric='cosine')
 
-def createGuidMap():
-	freebaseFile=open("./data/raw/filtered-freebase-simple-topic-dump-3cols.tsv","r")
-	guidMap={}
-	pattern=r'(/guid/[a-zA-Z0-9]*)([^/]*).*'
-	print pattern
-	for line in freebaseFile:
-		#print line
+    plt.figure(figsize=(125, 50))
+    plt.title('Hierarchical Clustering Dendrogram')
+    plt.xlabel('phrase')
+    plt.ylabel('distance')
+    dendrogram(Z, 
+            leaf_rotation=90.,  # rotates the x axis labels
+            leaf_font_size=8.,  # font size for the x axis labels
+            labels= phrase_sample
+            )
+    plt.show()
 
-		mo=re.match(pattern,line)
-		guid=mo.group(1)
-		ent=mo.group(2)
-		ent=ent.strip()
-		print guid
-		print ent
-		guidMap[guid]=ent
+    #plt.savefig('phrase_plot.png')    
+    # remove duplicates
+    #X_train_red = np.ascontiguousarray(X_train).view(np.dtype((np.void, X_train.dtype.itemsize * X_train.shape[1])))
+    #_, idx = np.unique(X_train_red, return_index=True)
+    #X_train_red = X_train[idx]
+    #y_label_red = y_label[idx]
 
-	pickle.dump(guidMap,open("./guidMap.p","wb"))
-
-def loadGuidMap():
-	return pickle.load(open("./guidMap.p","rb"))
-
-def createVectorMap():
-	vecFile= open("./data/raw/glove.6B/glove.6B.50d.txt","r")
-	vectorMap={}
-	for line in vecFile:
-		tokenList=line.split(" ")
-		word = tokenList[0] 
-		vector=[]
-		for value in tokenList[1:]:
-			vector.append(float(value))
-		vectorMap[word]=vector
-		# break
-		print word
-
-	pickle.dump(vectorMap,open("./vectorMap.p","wb"))
-
-	# for x in vectorMap["the"]:
-	# 	print x
-
-def loadVectorMap():
-	return pickle.load(open("./vectorMap.p","rb"))
-
-
-
-def writeBuckets_1(guidMap):
-	reload(sys)  
-	sys.setdefaultencoding('utf8')
-	rel=Document_pb2.Relation()
-	errorfile = open("err.txt","w")
-	bucketLabelFile=open("./data/processed/bucket_labels.tsv","w")
-	relIDFile=open("./data/processed/relationIDs.tsv","w")
-	relIDMap={}
-	relCount=0
-	for i in range(100000,119465):
-	# for i in range(100000,100002):
-		print i
-		inputFile= open("./data/raw/kb_manual/trainPositive/"+str(i)+".pb","rb")
-		rel.ParseFromString(inputFile.read())
-		#print rel
-		# break
-		#print rel.sourceGuid + " " + rel.destGuid 
-		inputFile.close()
-
-		relType=rel.relType
-		print relType
-		if relType not in relIDMap:
-			relIDFile.write(str(relCount)+"\t"+relType+"\n")
-			relIDMap[relType]=relCount
-			relCount+=1
-		
-		bucketLabelFile.write(str(relIDMap[relType])+"\n")
-
-		outputFile = open("./data/processed/buckets/b_"+str(i)+".tsv","w")
-		# print rel.sourceGuid
-		sourceEntity= guidMap[rel.sourceGuid]
-		# print sourceEntity
-		# print rel.destGuid
-		destEntity=guidMap[rel.destGuid]
-		# print destEntity
-
-		# sourceEntity="a"
-		# destEntity="b"
-
-
-		for mention in rel.mention :
-			sent=mention.sentence
-			#print mention.sourceId
-			# print sent
-			phrase1=re.match("(.*)"+sourceEntity+"(.*)"+destEntity+"(.*)",sent)
-			phrase2=re.match("(.*)"+destEntity+"(.*)"+sourceEntity+"(.*)",sent)
-			if phrase1:
-				phrase=phrase1.group(2)
-			elif phrase2:
-				phrase=phrase2.group(2)
-			else:
-				phrase = "Not FOUND"
-				errorfile.write(str(i)+"\n")
-			phrase=phrase.strip()
-			string =sourceEntity+"\t"+destEntity+"\t"+phrase+"\t"+sent+"\n"
-			outputFile.write(string);
-		outputFile.close()
-		# print rel
-	bucketLabelFile.close()
-	errorfile.close()
-	relIDFile.close()
-
-
-
-
-
-
-
-createGuidMap()
-guidMap=loadGuidMap()
-# guidMap=None
-writeBuckets_1(guidMap)
-# rel=Document_pb2.Relation()
-# rel.ParseFromString(open("./kb_manual/trainPositive/"+"100008"+".pb","rb").read())
-# sourceEntity= guidMap[rel.sourceGuid]
-# # print sourceEntity
-# # print rel.destGuid
-# destEntity=guidMap[rel.destGuid]
-# print rel.mention[0].sentence
-createVectorMap()
-print "My watch has ended"
-	
+    #count_list = []
+    #for key in buckets.keys():
+    #    count_list.append(buckets[key][-1])
+    #print "My watch has ended"
+                
