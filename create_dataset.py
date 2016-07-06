40213eff33567cc69025e33e324811c6db7c50e8
Aneesh Barthakur <abarthakur96@gmail.com>
2016-07-07 01:10:28 +0530

    queries working both ent and rel

--- create_dataset.py
+++ create_dataset.py
@@ -22,21 +22,21 @@ def checkForEntities(sent,sparql,idcounter):
     oldner="O"
     for ner in sent["ners"]:
         i+=1
-        print ner,i
+        # print ner,i
         if (oldner!=ner):
-            print "ner change",i
+            # print "ner change",i
             if (oldner in validNers):
                 #entity ended
-                print "ner end",i
+                # print "ner end",i
                 entMention={}
                 entMention["id"]=idcounter
                 idcounter+=1
                 entMention["from"]=startEntity
                 entMention["to"]=i-1
                 entMention["label"]=oldner
-                entMention["string"]=" ".join(sent["words"][startEntity-1:i-1])
-                print "New Entity  :  " + entMention["string"]
-                # checkEntityFreebase(entMention,sparql)
+                entMention["string"]=sent["words"][startEntity-1:i-1]
+                print "New Entity  :  " + str(entMention["string"])
+                checkEntityFreebase(entMention,sparql)
                 mentions.append(entMention)
             if ner in validNers:
                 startEntity=i
@@ -45,11 +45,24 @@ def checkForEntities(sent,sparql,idcounter):
     sent["mentions"]=mentions
 
 def checkEntityFreebase(entMention,sparql):
-    foundEntity=False
+    entMention["fbid"]=None
+    regex= " ".join(entMention["string"])
+    typeDict={"PERSON":":people.person","LOCATION":":location.location","ORGANISATION":":organization.organization"}
     #query freebase and set Guid if any. set to none else.
-    if not foundEntity:
-        entMention["guid"]=None
-    return
+    query = ('''prefix : <http://rdf.freebase.com/ns/>\n select ?entity ?entityname{
+            ?entity :type.object.name ?entityname .
+            ?entity a '''+typeDict[entMention["label"]]+" .\n"
+            '''filter regex(str(?entityname),"'''+regex+'''","i")\n'''
+            "} limit 1")
+    # print query
+    sparql.setQuery(query)
+    sparql.setReturnFormat(SPARQLWrapper.JSON)
+    results= sparql.query().convert()
+    for result in results["results"]["bindings"]:
+        print result["entity"]["value"],result["entityname"]["value"]
+        entMention["fbid"]=result["entity"]["value"]
+        entMention["fbname"]=result["entityname"]["value"]
+
 
 def extractFeatures(sent,relTuple):
     #extract features here. Note sdp algo can be reused easily
@@ -58,25 +71,32 @@ def extractFeatures(sent,relTuple):
 
 
 def checkForRelations(sent,sparql,file_name,sentCount,relations):
-
+    print "checking for relations"
     for i in range (0,len(sent["mentions"])):
         e1=sent["mentions"][i]
-        if not e1 :
+        if not e1["fbid"] :
             continue
         for j in range(i+1,len(sent["mentions"])):
-            foundRelation=False #dummy booleans later replace with proper condition
             e2=sent["mentions"][j]
-            if not e2 :
+            if not e2["fbid"] :
                 continue
             #query for which relations
+            query=('''prefix : <http://rdf.freebase.com/ns/>\nselect distinct ?rel {'''
+                    "<"+e1["fbid"]+"> ?rel <"+e2["fbid"]+">\n}")
+            sparql.setQuery(query)
+            sparql.setReturnFormat(SPARQLWrapper.JSON)
+            results= sparql.query().convert()
             #check if relations match training set
-            if foundRelation :
-                for rel in returnedRels:
-                    relTuple=(e1["guid"],rel,e2["guid"])
-                    sentence=" ".join(sent["words"])
-                    features=extractFeatures(sent,relTuple)
-                    relMention={"sourceId":e1["id"],"destId":e2["id"],"filename":file_name,"sentence":sentence,"features":features}
-                    relations[relTuple].append(relMention)
+            for result in results["results"]["bindings"]:
+                print result["rel"]["value"]
+                rel= result["rel"]["value"]
+                relTuple=(e1["fbid"],rel,e2["fbid"])
+                sentence=" ".join(sent["words"])
+                features=extractFeatures(sent,relTuple)
+                relMention={"sourceId":e1["id"],"destId":e2["id"],"filename":file_name,"sentence":sentence,"features":features}
+                if relTuple not in relations:
+                    relations[relTuple]=[]                    
+                relations[relTuple].append(relMention)
 
 
     return
@@ -84,7 +104,7 @@ def checkForRelations(sent,sparql,file_name,sentCount,relations):
 
 
 # ID FORM POSTAG NERTAG LEMMA DEPREL HEAD SENTID PROV.
-query = """ SELECT * FROM NAMED <http://freebase.com> WHERE {?s ?x ?o} limit 100 """
+# query = """ SELECT * FROM NAMED <http://freebase.com> WHERE {?s ?x ?o} limit 100 """
 sparql = SPARQLWrapper.SPARQLWrapper("http://172.16.116.93:8890/sparql/")
 # sparql.setQuery(query)
 # sparql.setReturnFormat(SPARQLWrapper.JSON)
@@ -108,7 +128,7 @@ for file_name in os.listdir(warc_file_directory):
                 sent["depTree"]=depTree
                 sent["depTreeRels"]=depTreeRels
                 checkForEntities(sent,sparql,idcounter)
-                # checkForRelations(sent,sparql,file_name,sentCount,relations)
+                checkForRelations(sent,sparql,file_name,sentCount,relations)
                 sentences.append(sent)
                 # print "old sentence :"+str(sent)            
             else :
@@ -143,6 +163,6 @@ for file_name in os.listdir(warc_file_directory):
     docCount+=1
 
 # relPickle=open("./data/raw/relations.p","wb")
-# # print "Relations"+str(relations)
+print "Relations"+str(relations)
 # pickle.dump(relations,relPickle)
 # relPickle.close()
